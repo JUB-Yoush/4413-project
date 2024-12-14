d9ed68a29a99eb755fb25c5a5f84b1456183b381
Greatlove Bariboloka <greatlebari@gmail.com>
2024-12-14 01:41:40 -0500

    - sign up form no longer expands to fill screen - postal code validation works

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -34,7 +34,7 @@ const findInputError = (errors: FieldErrors<FieldValues>, name: string) => {
 const InputError: React.FC<{message: string}> = ({message}) => {
     return (
         <div className="flex items-center gap-1 px-2 font-semibold text-pink text-sm bg-red-100">
-            {message}
+            <p className={" text-balance"}>{message}</p>
         </div>
     )
 }
